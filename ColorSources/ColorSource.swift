db43ac7b7675671a9e738b4f049dfce22e129835
jehanson4 <jehanson@gmail.com>
2018-04-17 13:03:16 -0400

    rewrite for clarity

--- ColorSources/ColorSource.swift
+++ ColorSources/ColorSource.swift
@@ -8,62 +8,17 @@
 
 import Foundation
 import GLKit
-#if os(iOS) || os(tvOS)
-import OpenGLES
-#else
-import OpenGL
-#endif
 
 // ==============================================================================
 // ==============================================================================
 
 protocol ColorSource {
     
-    static var type: String { get }
-    var name: String { get set }
-    var description: String { get set }
+    var name: String { get }
+    var description: String? { get set }
     
     func prepare()
     
-    func color(_ nodeIndex: Int) -> GLKVector4
-}
-
-protocol ColorSourceRegistry {
-    
-    var colorSourceNames: [String] { get }
-    
-    func getColorSource(_ name: String) -> ColorSource?
-
-    /// returns true iff the selection changed
-    func selectColorSource(_ name: String) -> Bool
-    
-    var selectedColorSource: ColorSource? { get }
-    
-}
-
-// ==============================================================================
-// ==============================================================================
-
-class ConstColor : ColorSource {
-    
-    static let type = "Const color"
-    var name = type
-    var description = type
-    
-    let color: GLKVector4
-    
-    init(r: GLfloat = 0, g: GLfloat = 0, b: GLfloat = 0) {
-        self.color = GLKVector4Make(r,g,b,1)
-    }
-    
-    init(_ color: GLKVector4) {
-        self.color = color
-    }
-    
-    func prepare() {}
-    
-    func color(_ nodeIndex: Int) -> GLKVector4 {
-        return color
-    }
+    func colorAt(_ nodeIndex: Int) -> GLKVector4
 }
 
---
+++ ColorSources/PhysicalPropertyColorSource.swift
@@ -0,0 +1,39 @@
+//
+//  PhysicalPropertyColorSource.swift
+//  SKT Visualizer
+//
+//  Created by James Hanson on 4/17/18.
+//  Copyright © 2018 James Hanson. All rights reserved.
+//
+
+import Foundation
+import GLKit
+
+// ===============================================================
+// PhysicalPropertyColorSource
+// ===============================================================
+
+class PhysicalPropertyColorSource : ColorSource {
+    
+    let name: String
+    
+    var description: String?
+    
+    var property: PhysicalProperty
+    var colorMap: ColorMap
+    
+    init(_ property: PhysicalProperty, colorMap: ColorMap, name: String? = nil, description: String? = nil) {
+        self.name = (name != nil) ? name! : property.name
+        self.description = (description != nil) ? description : property.description
+        self.property = property
+        self.colorMap = colorMap
+    }
+    
+    func prepare() {
+        colorMap.calibrate(property.bounds)
+    }
+    
+    func colorAt(_ nodeIndex: Int) -> GLKVector4 {
+        return colorMap.getColor(property.valueAt(nodeIndex: nodeIndex))
+    }
+}
