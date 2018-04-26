af693b18be95a308c3f37ec3f9666d43277e4984
jehanson4 <jehanson@gmail.com>
2018-04-25 23:16:24 -0400

    renaming

--- Visualization/Effects/TexturedFragmentShader.glsl
+++ Visualization/Effects/TexturedFragmentShader.glsl
@@ -1,6 +1,6 @@
 varying lowp vec4 frag_Color;
-uniform sampler2D u_texture;
+uniform sampler2D u_Texture;
 
 void main(void) {
-    gl_FragColor = texture2D(u_texture, gl_PointCoord) * frag_Color;
+    gl_FragColor = texture2D(u_Texture, gl_PointCoord) * frag_Color;
 }
